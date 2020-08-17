195acb9698ddc3bcb6b434a504c05cee9eb6c1ba
zztttt <1315691850@qq.com>
2020-08-17 21:05:24 +0800

    update weapon system

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -14,13 +14,29 @@ public class ButtonClick : MonoBehaviour
 
     private Text BagText;
 
-
+    private HashSet<KeyValuePair<int, int>> swordSet = new HashSet<KeyValuePair<int, int>>();
     private int count = 0;
     // Start is called before the first frame update
     void Start()
     {
         BagText = BagTextObj.GetComponent<Text>();
         BagText.text = "Count:" + count.ToString();
+
+        // generate sword
+        swordSet.Add(new KeyValuePair<int, int>(0, 5));
+        swordSet.Add(new KeyValuePair<int, int>(0, 6));
+        swordSet.Add(new KeyValuePair<int, int>(1, 4));
+        swordSet.Add(new KeyValuePair<int, int>(1, 6));
+        swordSet.Add(new KeyValuePair<int, int>(2, 3));
+        swordSet.Add(new KeyValuePair<int, int>(2, 5));
+        swordSet.Add(new KeyValuePair<int, int>(3, 1));
+        swordSet.Add(new KeyValuePair<int, int>(3, 2));
+        swordSet.Add(new KeyValuePair<int, int>(3, 4));
+        swordSet.Add(new KeyValuePair<int, int>(4, 2));
+        swordSet.Add(new KeyValuePair<int, int>(4, 3));
+        swordSet.Add(new KeyValuePair<int, int>(5, 1));
+        swordSet.Add(new KeyValuePair<int, int>(5, 3));
+        swordSet.Add(new KeyValuePair<int, int>(6, 0));
     }
 
     // Update is called once per frame
@@ -62,13 +78,63 @@ public class ButtonClick : MonoBehaviour
         
         Debug.Log("rows:" + rows+ ", cols:" + cols);
 
-        for (int i = 0; i < rows; ++i) {
+        /*for (int i = 0; i < rows; ++i) {
             for (int j = 0; j < cols; ++j) {
                 Image image = images[i * cols + j];
                 Color color = image.color;
                 if (color == Color.black)
                     Debug.Log("Black: row:" + i + ", col:" + j);
             }
+        }*/
+        if (isSword(images, rows, cols))
+        {
+            Debug.Log("this is sword");
         }
+        else {
+            Debug.Log("this is not sword");
+        }
+    }
+
+    public bool isSword(Image[] images, int rows, int cols) {
+        HashSet<KeyValuePair<int, int>> tmp = new HashSet<KeyValuePair<int, int>>(swordSet);
+        int blackCount = 0;
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                Image image = images[i * cols + j];
+                Color color = image.color;
+                if (color == Color.black)
+                    blackCount++;
+            }
+        }
+
+        if (blackCount != 14) {
+            Debug.Log("count not correct");
+            return false;
+        }
+
+        for (int i = 0; i < rows; ++i)
+        {
+            for (int j = 0; j < cols; ++j)
+            {
+                Image image = images[i * cols + j];
+                Color color = image.color;
+                if (color == Color.black)
+                {
+                    KeyValuePair<int, int> kv = new KeyValuePair<int, int>(i, j);
+                    if (tmp.Contains(kv))
+                    {
+                        tmp.Remove(kv);
+                    }
+                    else {
+                        Debug.Log("color not correct");
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return tmp.Count == 0 ? true:false;
     }
 }
---
+++ Assets/Scripts/hunterController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hunterController : MonoBehaviour
+{
+    //private Rigidbody2D rigidbody;
+    //private BoxCollider2D collider;
+    private Animator animator;
+
+    public GameObject swordImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody = GetComponent<Rigidbody2D>();
+        //collider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("hunter: OnCollisionEnter2D");
+
+        animator.SetTrigger("talk");
+        if (swordImage.activeSelf == false) {
+            swordImage.SetActive(true);
+            Invoke("showSwordImageEnd", 5f);
+        }
+    }
+
+    private void showSwordImageEnd() {
+        swordImage.SetActive(false);
+    }
+}
